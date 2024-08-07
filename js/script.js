8f00483bbb60411db1f7eadcacb49958f40f12ba
Arley Augusto Aguiar <arley.it@icloud.com>
2024-08-07 20:34:46 -0300

    exercicio_02_verificador_de_idade

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getUTCFullYear()
+    var fano = document.getElementById('txano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.lenght == 0 || fano.value > ano) {
+        window.alert('[Erro] verifique os dados e tente novamente!!')
+    }
+}
